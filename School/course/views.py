35846a19df6152a3ae1d4271da7ee1ff4cf6970c
M Farhan ur Rasool <145224436+farhanjutt23@users.noreply.github.com>
2024-08-04 20:23:23 +0500

    Practice django

---
+++ School/course/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def django_learing(slef):
+    return HttpResponse("<h1>This is the my file of the code <h1>")
+def python_learing(self):
+    return HttpResponse("This is the our file of the python")
