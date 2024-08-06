ab429ad46609fef57371fe5c751548bde21d0788
M Farhan ur Rasool <145224436+farhanjutt23@users.noreply.github.com>
2024-08-07 00:03:39 +0500

    IF tag

---
+++ Day_9 If and if else and if elif tags in the templates.html/school/course/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def django_learing(self):
+    cname='django'
+    duration='4 weeks'
+    django_details={'cn': cname, 'du': duration}
+    return render(self,'course/course_one.html', django_details)
+
