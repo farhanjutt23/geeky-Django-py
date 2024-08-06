a7ba850a4351bbb0113dec341aeb49602dc73746
M Farhan ur Rasool <145224436+farhanjutt23@users.noreply.github.com>
2024-08-07 00:19:25 +0500

    Delete Day_10 For loop in the templates.html/school directory

--- Day_10 For loop in the templates.html/school/course/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def django_learing(self):
-    student={'names':['Rahul','Farhan','Ali','Ahmad']}
-    return render(self,'course/course_one.html', student)
