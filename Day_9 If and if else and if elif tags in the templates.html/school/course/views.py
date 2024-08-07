ee311c678ecdf71e6b6fae601b8571ef106cf2af
M Farhan ur Rasool <145224436+farhanjutt23@users.noreply.github.com>
2024-08-07 21:26:36 +0500

    Delete Day_9 If and if else and if elif tags in the templates.html/school directory

--- Day_9 If and if else and if elif tags in the templates.html/school/course/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def django_learing(self):
-    cname='django'
-    duration='4 weeks'
-    django_details={'cn': cname, 'du': duration}
-    return render(self,'course/course_one.html', django_details)
-
