29ce40b1125ef9097cbe01fe554f70f998a0c289
M Farhan ur Rasool <145224436+farhanjutt23@users.noreply.github.com>
2024-08-07 21:24:49 +0500

    Delete Day_5 Dynamic_Template/school directory

--- Day_5 Dynamic_Template/school/fee/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def fee_django(self):
-    return render(self,'fee/fee_one.html')
-def fee_python(self):
-    return render(self,'fee/fee_two.html')
