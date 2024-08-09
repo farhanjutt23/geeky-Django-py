f92bf79b2cddeb5304233dcc909196e5a091979d
M Farhan ur Rasool <145224436+farhanjutt23@users.noreply.github.com>
2024-08-09 21:23:40 +0500

    Templates

---
+++ 19 Days inculde tag in the django/school/core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(self):
+    return render(self,'core/home.html')
+def about(self):
+    return render(self,'core/about.html')
